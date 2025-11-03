3b8e8ce100442dcba5c42d0f7da3b0cb4de4b630
Bangraknoi <bangraknoi2022@gmail.com>
2025-11-03 10:38:50 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// LIVE-PRODUCTION-FINAL v2
+window.AppConfig = {
+  storageType: 'firebase',
+  firebase: {
+    apiKey: "AIzaSyCDRWHl9MBNCwvAf845Tj4QRWhyNnHVoY0",
+    authDomain: "loykrathong-bangraknoi.firebaseapp.com",
+    projectId: "loykrathong-bangraknoi",
+    storageBucket: "loykrathong-bangraknoi.firebasestorage.app",
+    messagingSenderId: "878082035827",
+    appId: "1:878082035827:web:185f4e67b6fde8d36d9a06"
+  }
+};
